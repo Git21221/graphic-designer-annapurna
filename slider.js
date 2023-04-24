5c352a8032636cb9655efa6968afdf7c74f7c17d
Your Name <you@example.com>
2023-04-24 18:59:05 +0530

    partially updated

---
+++ slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var slideWidth = $('.slider').width() / 2;
+
+    $('.slides-container').css('transform', 'translateX(-' + slideWidth + 'px)');
+
+    $('.slider').on('click', '.prev', function (e) {
+        e.preventDefault();
+        $('.slides-container').animate({
+            left: '+=' + slideWidth
+        }, 500, function () {
+            $('.slide:last-child').prependTo('.slides-container');
+            $('.slides-container').css('left', -slideWidth);
+        });
+    });
+
+    $('.slider').on('click', '.next', function (e) {
+        e.preventDefault();
+        $('.slides-container').animate({
+            left: '-=' + slideWidth
+        }, 500, function () {
+            $('.slide:first-child').appendTo('.slides-container');
+            $('.slides-container').css('left', -slideWidth);
+        });
+    });
+});
